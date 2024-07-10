15a373d1df2f48df5ba45409b2b743b4234023e2
signOfI <alam14@seas.upenn.edu>
2024-07-10 14:21:06 -0700

    Update .gitignore to ignore environment files

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  googleMapsApiKey: 'AIzaSyAVVYR6M-gD1D5CpKHPvA_-zZdr2EpMSdw'
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  googleMapsApiKey: 'AIzaSyAVVYR6M-gD1D5CpKHPvA_-zZdr2EpMSdw'
-};
